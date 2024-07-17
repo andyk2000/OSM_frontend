8625c0589d20a9b7f7194cfb62bdaa2f77e1157a
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-07-17 11:32:32 +0200

    fix: implemented streaming of data for the update page

--- src/app/merchant/store/edit/page.tsx
+++ src/app/merchant/store/edit/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState, Suspense, ChangeEvent } from "react";
 import styles from "./page.module.css";
 import { Icon } from "@iconify/react";
 import * as Yup from "yup";
@@ -19,8 +19,8 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { EditPhotoSkeleton } from "../../skeleton/editDashboard";
 
-export default function Edit() {
-  const [store, setStore] = useState({
+const UpdateForm = () => {
+  const [store, setStore] = useState<NewStore>({
     name: "",
     address: "",
     description: "",
@@ -30,14 +30,14 @@ export default function Edit() {
   });
 
   const [preview, setPreview] = useState<string>("");
-  const [logoFile, setLogoFile] = useState("");
-  const [storeLink, setStoreLink] = useState("");
-  const [storeName, setStoreName] = useState("");
-  const [imageChange, setImageChange] = useState(false);
+  const [logoFile, setLogoFile] = useState<string>("");
+  const [storeLink, setStoreLink] = useState<string>("");
+  const [, setStoreName] = useState<string>("");
+  const [imageChange, setImageChange] = useState<boolean>(false);
 
   const validationSchema = Yup.object({
     name: Yup.string().required("* name is required"),
-    phone: Yup.string().required("* contact of your store are required"),
+    phone: Yup.string().required("* contact of your store is required"),
     address: Yup.string().required("* the address is required"),
     email: Yup.string()
       .email("* Invalid email address")
@@ -47,19 +47,15 @@ export default function Edit() {
     ),
   });
 
-  const handleImageChange = (event: {
-    target?: { dispatchEvent: (arg0: Event) => void };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    currentTarget?: any;
-  }) => {
-    const file = event.currentTarget.files[0];
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.currentTarget.files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setLogoFile(reader.result as string);
-        setPreview(reader.result as string);
+        const result = reader.result as string;
+        setLogoFile(result);
+        setPreview(result);
         setImageChange(true);
-        console.log(preview);
       };
       reader.readAsDataURL(file);
     } else {
@@ -76,14 +72,13 @@ export default function Edit() {
     try {
       const id = parseInt(searchParams.get("id") || "");
       const updatedStore = await updateStore(id, updatedStoreData, imageChange);
-      if (updatedStore.success === true) {
+      if (updatedStore.success) {
         successUpdate();
       } else {
         failedUpdate();
       }
     } catch (error) {
-      console.log(error);
-      throw error;
+      console.error(error);
     }
   };
 
@@ -99,18 +94,15 @@ export default function Edit() {
       confirmButtonText: "Yes, delete it!",
     }).then(async (result) => {
       if (result.isConfirmed) {
-        if (result.isConfirmed) {
-          try {
-            const storeDeleted = await deleteStore(id);
-            if (storeDeleted.success === true) {
-              successDelete();
-            } else {
-              failedDelete();
-            }
-          } catch (error) {
-            console.log(error);
-            throw error;
+        try {
+          const storeDeleted = await deleteStore(id);
+          if (storeDeleted.success) {
+            successDelete();
+          } else {
+            failedDelete();
           }
+        } catch (error) {
+          console.error(error);
         }
       }
     });
@@ -119,7 +111,7 @@ export default function Edit() {
   const successDelete = () => {
     Swal.fire({
       title: "Delete Successful",
-      text: "Store has been deleted Successfully",
+      text: "Store has been deleted successfully",
       icon: "success",
       confirmButtonColor: "#3e61ac",
       confirmButtonText: "Proceed!",
@@ -156,10 +148,6 @@ export default function Edit() {
     });
   };
 
-  const goBack = () => {
-    navigateStorePage();
-  };
-
   const searchParams = useSearchParams();
 
   useEffect(() => {
@@ -177,22 +165,222 @@ export default function Edit() {
             address: results.data.address,
             logo: "",
           });
-          if (typeof results.data?.storeUrl === "string") {
-            setStoreLink(results.data.storeUrl);
-          }
-          if (typeof results.data?.name === "string") {
-            setStoreName(results.data.name);
-          }
-          if (typeof results.data?.logo === "string") {
-            setPreview(results.data.logo);
-          }
+          setStoreLink(results.data.storeUrl || "");
+          setStoreName(results.data.name || "");
+          setPreview(results.data.logo || "");
         }
       } else {
-        console.log("There was a problem retrieving the data.");
+        console.error("There was a problem retrieving the data.");
       }
     };
     initialData();
   }, [searchParams]);
+
+  return (
+    <Formik
+      initialValues={store}
+      validationSchema={validationSchema}
+      enableReinitialize={true}
+      onSubmit={submitStore}
+    >
+      {() => (
+        <Form className={styles.storeForm}>
+          <div className={styles.photoInput}>
+            <div
+              className={clsx(styles.photoFrameActive, {
+                [styles.photoFrame]: preview.length > 0,
+              })}
+            >
+              <Icon
+                icon="ph:camera"
+                style={{ color: "rgb(62, 97, 172)" }}
+                height={50}
+                width={50}
+              />
+            </div>
+            {preview ? (
+              <Image
+                src={preview}
+                alt="Preview"
+                className={clsx(styles.storeLogo, {
+                  [styles.storeLogoActive]: preview.length > 0,
+                })}
+                width={100}
+                height={100}
+              />
+            ) : (
+              <div className={styles.storeLogoPlaceholder}>No Image</div>
+            )}
+            <Field
+              dot={false}
+              id="logo"
+              name="logo"
+              type="file"
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                handleImageChange(event);
+                event.target.dispatchEvent(
+                  new Event("input", { bubbles: true }),
+                );
+              }}
+              className={styles.imageInput}
+            />
+          </div>
+          <div className={styles.linkSpace}>
+            <p>Store Url:</p>
+            <Icon
+              icon="ph:link"
+              style={{ color: "rgba(62, 97, 172)" }}
+              height={40}
+              width={20}
+            />
+            <Link href={storeLink} className={styles.storeURLText}>
+              {storeLink}
+            </Link>
+          </div>
+          <div className={styles.dualFieldSpace}>
+            <div>
+              <div className={styles.fieldLabel}>
+                <Icon
+                  icon="ph:storefront-light"
+                  style={{ color: "rgb(62, 97, 172)" }}
+                  height={25}
+                  width={25}
+                />
+                <p>
+                  Store Name<span>*</span>
+                </p>
+              </div>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Store Name"
+                className={styles.fieldInput}
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+            <div>
+              <div className={styles.fieldLabel}>
+                <Icon
+                  icon="ph:envelope-open-light"
+                  style={{ color: "rgb(62, 97, 172)" }}
+                  height={25}
+                  width={25}
+                />
+                <p>
+                  Email<span>*</span>
+                </p>
+              </div>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                className={styles.fieldInput}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+          </div>
+          <div className={styles.dualFieldSpace}>
+            <div>
+              <div className={styles.fieldLabel}>
+                <Icon
+                  icon="ph:map-pin-simple-fill"
+                  style={{ color: "rgb(62, 97, 172)" }}
+                  height={25}
+                  width={25}
+                />
+                <p>
+                  Address<span>*</span>
+                </p>
+              </div>
+              <Field
+                type="text"
+                id="address"
+                name="address"
+                placeholder="address"
+                className={styles.fieldInput}
+              />
+              <ErrorMessage
+                name="address"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+            <div>
+              <div className={styles.fieldLabel}>
+                <Icon
+                  icon="ph:phone-light"
+                  style={{ color: "rgb(62, 97, 172)" }}
+                  height={25}
+                  width={25}
+                />
+                <p>
+                  Phone Number<span>*</span>
+                </p>
+              </div>
+              <Field
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="0780000000"
+                className={styles.fieldInput}
+              />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+          </div>
+          <div className={styles.descriptionContainer}>
+            <p>Store Description</p>
+            <Field
+              as="textarea"
+              id="description"
+              name="description"
+              rows="5"
+              className={styles.textarea}
+            />
+            <ErrorMessage
+              name="description"
+              component="div"
+              className={styles.errorMessage}
+            />
+          </div>
+          <div className={styles.buttonsField}>
+            <button type="submit" className={styles.publishButton}>
+              Publish
+            </button>
+            <button type="button" className={styles.saveButton}>
+              Revert Changes
+            </button>
+            <button
+              type="button"
+              className={styles.deleteButton}
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const Edit = () => {
+  const goBack = () => {
+    navigateStorePage();
+  };
 
   return (
     <div className={styles.pageContainer}>
@@ -205,7 +393,7 @@ export default function Edit() {
             width={30}
           />
         </button>
-        <h2 className={styles.pageTitle}>{storeName}</h2>
+        <h2 className={styles.pageTitle}></h2>
         <Icon
           icon="ph:dots-three-outline-vertical-fill"
           style={{ color: "rgba(62, 97, 172, 0.7)" }}
@@ -213,205 +401,11 @@ export default function Edit() {
           width={25}
         />
       </div>
-      <Formik
-        initialValues={store}
-        validationSchema={validationSchema}
-        enableReinitialize={true}
-        onSubmit={submitStore}
-      >
-        {() => (
-          <Form className={styles.storeForm}>
-            <Suspense fallback={<EditPhotoSkeleton />}>
-              <div className={styles.photoInput}>
-                <div
-                  className={clsx(styles.photoFrameActive, {
-                    [styles.photoFrame]: preview.length > 0,
-                  })}
-                >
-                  <Icon
-                    icon="ph:camera"
-                    style={{ color: "rgb(62, 97, 172)" }}
-                    height={50}
-                    width={50}
-                  />
-                </div>
-                {preview ? (
-                  <Image
-                    src={preview}
-                    alt="Preview"
-                    className={clsx(styles.storeLogo, {
-                      [styles.storeLogoActive]: preview.length > 0,
-                    })}
-                    width={100}
-                    height={100}
-                  />
-                ) : (
-                  <div className={styles.storeLogoPlaceholder}>No Image</div>
-                )}
-                <Field
-                  dot={false}
-                  id="logo"
-                  name="logo"
-                  type="file"
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    handleImageChange(event);
-                    event.target.dispatchEvent(
-                      new Event("input", { bubbles: true }),
-                    );
-                  }}
-                  className={styles.imageInput}
-                />
-              </div>
-            </Suspense>
-            <div className={styles.linkSpace}>
-              <p>Store Url:</p>
-              <Icon
-                icon="ph:link"
-                style={{ color: "rgba(62, 97, 172)" }}
-                height={40}
-                width={20}
-              />
-              <Link href={storeLink} className={styles.storeURLText}>
-                {storeLink}
-              </Link>
-            </div>
-            <div className={styles.dualFieldSpace}>
-              <div>
-                <div className={styles.fieldLabel}>
-                  <Icon
-                    icon="ph:storefront-light"
-                    style={{ color: "rgb(62, 97, 172)" }}
-                    height={25}
-                    width={25}
-                  />
-                  <p>
-                    Store Name<span>*</span>
-                  </p>
-                </div>
-                <Field
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Store Name"
-                  className={styles.fieldInput}
-                />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className={styles.errorMessage}
-                />
-              </div>
-              <div>
-                <div className={styles.fieldLabel}>
-                  <Icon
-                    icon="ph:envelope-open-light"
-                    style={{ color: "rgb(62, 97, 172)" }}
-                    height={25}
-                    width={25}
-                  />
-                  <p>
-                    Email<span>*</span>
-                  </p>
-                </div>
-                <Field
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="email"
-                  className={styles.fieldInput}
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className={styles.errorMessage}
-                />
-              </div>
-            </div>
-            <div className={styles.dualFieldSpace}>
-              <div>
-                <div className={styles.fieldLabel}>
-                  <Icon
-                    icon="ph:map-pin-simple-fill"
-                    style={{ color: "rgb(62, 97, 172)" }}
-                    height={25}
-                    width={25}
-                  />
-                  <p>
-                    Address<span>*</span>
-                  </p>
-                </div>
-                <Field
-                  type="text"
-                  id="address"
-                  name="address"
-                  placeholder="address"
-                  className={styles.fieldInput}
-                />
-                <ErrorMessage
-                  name="address"
-                  component="div"
-                  className={styles.errorMessage}
-                />
-              </div>
-              <div>
-                <div className={styles.fieldLabel}>
-                  <Icon
-                    icon="ph:phone-light"
-                    style={{ color: "rgb(62, 97, 172)" }}
-                    height={25}
-                    width={25}
-                  />
-                  <p>
-                    Phone Number<span>*</span>
-                  </p>
-                </div>
-                <Field
-                  type="text"
-                  id="phone"
-                  name="phone"
-                  placeholder="0780000000"
-                  className={styles.fieldInput}
-                />
-                <ErrorMessage
-                  name="phone"
-                  component="div"
-                  className={styles.errorMessage}
-                />
-              </div>
-            </div>
-            <div className={styles.descriptionContainer}>
-              <p>Store Description</p>
-              <Field
-                as="textarea"
-                id="description"
-                name="description"
-                rows="5"
-                className={styles.textarea}
-              />
-              <ErrorMessage
-                name="description"
-                component="div"
-                className={styles.errorMessage}
-              />
-            </div>
-            <div className={styles.buttonsField}>
-              <button type="submit" className={styles.publishButton}>
-                Publish
-              </button>
-              <button type="button" className={styles.saveButton}>
-                Revert Changes
-              </button>
-              <button
-                type="button"
-                className={styles.deleteButton}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+      <Suspense fallback={<EditPhotoSkeleton />}>
+        <UpdateForm />
+      </Suspense>
     </div>
   );
-}
+};
+
+export default Edit;
